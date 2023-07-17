e6be330478be4d34c827bafcf92ded1fe8936571
cattag <acatcalledjordan@gmail.com>
2023-07-17 13:53:43 +1000

    added stick and wood pic crafting

--- src/textmc.java
+++ src/textmc.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 import java.util.Random;
-
+//test
 public class textmc {
     public static void main(String[] args){
         System.out.println("#=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#");
